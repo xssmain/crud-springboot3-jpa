e20bac085eb4d5968df25ad454ad4959000275d6
xssmain <whazeleyes@gmail.com>
2025-03-13 11:45:19 -0300

    h2 database, test profile, JPA

--- course/src/main/java/com/xssmain/course/entities/User.java
+++ course/src/main/java/com/xssmain/course/entities/User.java
@@ -1,17 +1,25 @@
 package com.xssmain.course.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
     private String phone;
     private String password;
 
-    public User() {}
+    public User() {
+    }
 
     public User(Long id, String name, String email, String phone, String password) {
         this.id = id;
